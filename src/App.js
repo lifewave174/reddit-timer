9812cd7ecf266cd91ac461f03049e20fb3a31dde
dci <priya.shah174@gmail.com>
2021-08-23 16:17:38 +0200

    added router with routes / and /search &  set up file structure for home, search and header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 import './Normalize.css';
 import './App.css';
+
+import Header from './components/Header';
+import Search from './components/Search';
+import Home from './components/Home';
 
 function App() {
   return (
     <div>
       App Placeholder
+      <Router>
+        <Header />
+        <div>
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route exact path="/search"><Search /></Route>
+          </Switch>
+
+        </div>
+      </Router>
     </div>
   );
 }
