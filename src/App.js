f1c8028862254da5bb92e647f5d8480cf587e5b9
Priya <58366904+lifewave174@users.noreply.github.com>
2021-08-28 18:18:37 +0200

    Create app skeleton (#1)
    * initialized css, reset with normalize.css & imported Bitter and Montserrat in App.css
    
    * added router with routes / and /search &  set up file structure for home, search and header
    
    * added global styles
    
    * fixed errors
    
    * changed basic info from create-react-app in index.html such as title and description
    
    * installed styled-components
    
    * installed fonts as well
    
    * set up globalstyled with styled components
    
    * set up theme.js for the theme object
    
    * added boilerplate code for NoMatch.js
    
    * changed manifest.json and added redit timer favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './theme';
+import GlobalStyle from './GlobalStyle';
+
+import Header from './components/Header';
+import Search from './components/Search';
+import Home from './components/Home';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
-    <div>
-      App Placeholder
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Normalize />
+          <GlobalStyle />
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/NoMatch.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NoMatch = () => <div />;
+
+export default NoMatch;
