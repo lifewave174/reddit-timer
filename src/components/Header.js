b34ab194fd4b445117e277ae9db906bd923a1cea
dci <priya.shah174@gmail.com>
2021-09-02 16:27:15 +0200

    imported logo as svg and added it in Header.js. Also created wrapper styled component in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Logo } from '../assets/logo.svg';
 
-const Header = () => <div />;
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <Logo />
+        </Wrapper>
+    );
+};
 
 export default Header;
