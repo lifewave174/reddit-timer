a84a1c9586f871f7fc86fc50f4e5d4f05e091fdc
dci <priya.shah174@gmail.com>
2021-10-27 16:44:20 +0200

    styled the hero title in hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,22 +9,36 @@ const Section = styled.section`
     align-items: center;
 `;
 
+const TitleSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
 const HeroImage = styled.div`
     background-image: url(${heroImage});
     height: 332px;
     width: 1114px;
 `;
 
+const H1 = styled.h1`
+    font-family: ${props => props.theme.font.family.headline};
+    color: #000000;
+    font-weight: 400;
+    font-size: 38px;
+`;
+
 const Hero = () => {
     return (
         <Section>
-            <div>
-                <h1>No reactions to your reddit posts?</h1>
+            <TitleSection>
+                <H1>No reactions to your reddit posts?</H1>
                 <h3>
                     Great timing, great results! Find the best time to post on
                     your subreddit
                 </h3>
-            </div>
+            </TitleSection>
             <button>Show me the best time</button>
             <p>r/javascript</p>
             <HeroImage />
