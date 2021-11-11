68253a7f40b4a26d877a67f5ef02237639059a07
dci <priya.shah174@gmail.com>
2021-11-11 11:01:42 +0100

    added a finally in the getTopPosts function

--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -36,34 +36,41 @@ const SearchPage = () => {
         let url = `https://www.reddit.com/r/${postSearch.subreddit}/top.json?t=${postSearch.time}&limit=${postSearch.limit}`;
         let _topPosts = [];
         let after = '';
-        while (_topPosts.length < 500 && after !== null) {
-            const apiData = await fetch(url);
-            const _data = await handleResponse(apiData);
-            if (_data.after) {
-                url = `https://www.reddit.com/r/${postSearch.subreddit}/top.json?t=${postSearch.time}&limit=${postSearch.limit}&after=${_data.after}`;
-            }
-            after = _data.after;
+        try {
+            while (_topPosts.length < 500 && after !== null) {
+                const apiData = await fetch(url);
+                const _data = await handleResponse(apiData);
+                if (_data.after) {
+                    url = `https://www.reddit.com/r/${postSearch.subreddit}/top.json?t=${postSearch.time}&limit=${postSearch.limit}&after=${_data.after}`;
+                }
+                after = _data.after;
 
-            _data.children.map(post => {
-                _topPosts.push({
-                    title: (
-                        <a href={post.data.permalink} target="_blank">
-                            {post.data.title}
-                        </a>
-                    ),
-                    time: new Date(post.data.created_utc * 1000)
-                        .toTimeString()
-                        .substring(0, 2),
-                    score: post.data.score,
-                    comments: post.data.num_comments,
-                    author: post.data.author,
-                    day: days[new Date(post.data.created_utc * 1000).getDay()],
+                _data.children.map(post => {
+                    _topPosts.push({
+                        title: (
+                            <a href={post.data.permalink} target="_blank">
+                                {post.data.title}
+                            </a>
+                        ),
+                        time: new Date(post.data.created_utc * 1000)
+                            .toTimeString()
+                            .substring(0, 2),
+                        score: post.data.score,
+                        comments: post.data.num_comments,
+                        author: post.data.author,
+                        day: days[
+                            new Date(post.data.created_utc * 1000).getDay()
+                        ],
+                    });
                 });
-            });
 
-            setTopPosts(_topPosts);
+                setTopPosts(_topPosts);
+            }
+        } catch {
+            handleError;
+        } finally {
+            setLoading(false);
         }
-        setLoading(false);
     };
 
     console.log(topPosts);
